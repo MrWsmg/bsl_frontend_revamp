e798e25ba2391c3b3f4ab9955a13326515c686ca
MrWsmg <waltermkingilima96@gmail.com>
2026-07-15 15:13:05 +0300

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://bsl-238481219614.europe-west1.run.app/api/:path*", // Proxy to Backend
+        destination: "https://bsl-238481219614.europe-west1.run.app/api/:path*", // Proxy to Backend on 
         //destination: "http://localhost:8000/api/:path*", // Proxy to local backend
       },
     ];
